2191fd88702b79f1aac428a99b10d653215869f8
Taynara Simoes <taynara.simoes@gruposbf.com.br>
2021-04-26 18:01:04 -0300

    Updating Program.cs file

--- bank/Program.cs
+++ bank/Program.cs
@@ -6,7 +6,11 @@ namespace bank
     {
         static void Main(string[] args)
         {
-            
+            public string titularConta = "Taynara";
+            public int numeroAgencia = 1234;
+            public int numeroConta = 67890;
+            public double saldoConta = 10;
+
         }
     }
 }
