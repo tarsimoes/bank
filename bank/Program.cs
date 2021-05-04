99162e230433b9e0d62537fcfb3901fb23877188
Felipe Garcia Fogal <felipe.fogal@gruposbf.com.br>
2021-05-04 17:58:55 -0300

    Criando classe de Conta Corrente para separar as dependencias

--- bank/Program.cs
+++ bank/Program.cs
@@ -6,11 +6,21 @@ namespace bank
     {
         static void Main(string[] args)
         {
-            public string titularConta = "Taynara";
-            public int numeroAgencia = 1234;
-            public int numeroConta = 67890;
-            public double saldoConta = 10;
+            ContaCorrente contaCorrente = new ContaCorrente();
+            contaCorrente.titularConta = "Fogal";
+            contaCorrente.numeroAgencia = 1234;
+            contaCorrente.numeroConta = 123456;
+            contaCorrente.saldoConta = 1234.00;
 
+            ContaCorrente contaCorrente2 = new ContaCorrente();
+            contaCorrente2.titularConta = "Tay";
+            contaCorrente2.numeroAgencia = 2233;
+            contaCorrente2.numeroConta = 332255;
+            contaCorrente2.saldoConta = 20.00;
+
+            Console.WriteLine($"O nome do titular da é {contaCorrente.titularConta}");
+            Console.WriteLine($"O nome do segundo titular é {contaCorrente2.titularConta}");
+            
         }
     }
 }
